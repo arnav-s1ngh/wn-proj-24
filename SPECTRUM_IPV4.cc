5f93f07d739ca6a403851233f5dc626df00a682a
arnav-s1ngh <92050142+arnav-s1ngh@users.noreply.github.com>
2024-12-15 23:24:27 +0530

    Update SPECTRUM_IPV4.cc

--- SPECTRUM_IPV4.cc
+++ SPECTRUM_IPV4.cc
@@ -1,96 +1,10 @@
-#include "ns3/applications-module.h"
-#include "ns3/core-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/network-module.h"
-#include "ns3/packet-sink.h"
-#include "ns3/wifi-module.h"
-#include "ns3/mobility-module.h"
-#include "ns3/netanim-module.h"
-#include "ns3/point-to-point-module.h"
-#include <fstream>
-#include <string>
-
-
-#include "ns3/attribute-container.h"
-#include "ns3/boolean.h"
-#include "ns3/command-line.h"
-#include "ns3/config.h"
-#include "ns3/double.h"
-#include "ns3/enum.h"
-#include "ns3/he-phy.h"
-#include "ns3/internet-stack-helper.h"
-#include "ns3/ipv4-address-helper.h"
-#include "ns3/ipv4-global-routing-helper.h"
-#include "ns3/log.h"
-#include "ns3/mobility-helper.h"
-#include "ns3/multi-model-spectrum-channel.h"
-#include "ns3/on-off-helper.h"
-#include "ns3/packet-sink-helper.h"
-#include "ns3/packet-sink.h"
-#include "ns3/spectrum-wifi-helper.h"
-#include "ns3/ssid.h"
-#include "ns3/string.h"
-#include "ns3/udp-client-server-helper.h"
-#include "ns3/udp-server.h"
-#include "ns3/uinteger.h"
-#include "ns3/wifi-acknowledgment.h"
-#include "ns3/yans-wifi-channel.h"
-#include "ns3/yans-wifi-helper.h"
- 
 #include <algorithm>
 #include <functional>
 
 using namespace ns3;
 NS_LOG_COMPONENT_DEFINE("WN_Assign-3");
 
-// Response Time
-int packet_cnt=0;
-double resp_time=0;
-
-// Collision and reception counters
-int totalreceptions=0;
-int totalcollisions=0;
-
-// Track request and response times
-std::map<uint32_t,Time> packetsendtimes; // Map packet ID to send time
-
-void PacketReception(std::string context,Ptr<const Packet> packet,double snr,WifiMode mode,WifiPreamble preamble) {
-    totalreceptions++;
-}
-
-void PacketCollision(std::string context,Ptr<const Packet> packet,double snr) {
-    totalcollisions++;
-}
-
-// Callback for PHY transmission
-void PhyTxTrace(std::string context,Ptr<const Packet> packet,WifiMode mode,WifiPreamble preamble,uint8_t txPower) {
-    // Store the transmission time for each packet based on its ID
-    packetsendtimes[packet->GetUid()]=Simulator::Now();
-}
-
-// Callback for PHY reception
-void PhyRxOkTrace(std::string context,Ptr<const Packet> packet,double snr,WifiMode mode,WifiPreamble preamble) {
-    uint32_t packetid=packet->GetUid();
-    
-    if (packetsendtimes.find(packetid) != packetsendtimes.end()) {
-        // Calculate the response time
-        Time sendtime=packetsendtimes[packetid];
-        Time receivetime=Simulator::Now();
-        Time responsetime=receivetime-sendtime;
-        
-        //std::cout << "Packet ID: " << packetid << ",Response time: " << responsetime.GetMicroSeconds() << " microseconds" << std::endl;
-        resp_time+=responsetime.GetMicroSeconds();
-        packet_cnt+=1;
-        
-        // Remove the entry to keep the map clean
-        packetsendtimes.erase(packetid);
-    }
-}
-
 void simulation(int zis, std::string tcp_variant) {
-    //Config::SetDefault("ns3::WifiDefaultAckManager::DlMuAckSequenceType",EnumValue(WifiAcknowledgment::DL_MU_BAR_BA_SEQUENCE));
-    //Config::SetDefault("ns3::WifiDefaultAckManager::DlMuAckSequenceType",EnumValue(WifiAcknowledgment::DL_MU_TF_MU_BAR));
-    //Config::SetDefault("ns3::WifiDefaultAckManager::DlMuAckSequenceType",EnumValue(WifiAcknowledgment::DL_MU_AGGREGATE_TF));
     int nw=20; // Number of STAs in the WiFi network
     NodeContainer sta_nodes;
     sta_nodes.Create(nw);
@@ -168,7 +82,6 @@ void simulation(int zis, std::string tcp_variant) {
     sta_interface=address.Assign(sta_devices);
     
     int bs_port=9;
-    int oo_port=40;
     
     for (int i=0; i < 20; i++) {
         BulkSendHelper source("ns3::TcpSocketFactory",InetSocketAddress(sta_interface.GetAddress(i),bs_port));
@@ -184,26 +97,11 @@ void simulation(int zis, std::string tcp_variant) {
         bs_port+=1;
     }
     Ipv4GlobalRoutingHelper::PopulateRoutingTables(); 
-    p2pconn.EnablePcapAll("wired_capture",false);
-    phy.EnablePcapAll("wireless_capture",false);
-
-    // Connect to reception and collision events
-    Config::Connect("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Phy/State/RxOk",MakeCallback(&PacketReception));
-    Config::Connect("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Phy/State/RxError",MakeCallback(&PacketCollision));
-    
-    // Connect to PHY Tx and RxOk events for response time calculation
-    Config::Connect("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Phy/State/Tx",MakeCallback(&PhyTxTrace));
-    Config::Connect("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Phy/State/RxOk",MakeCallback(&PhyRxOkTrace));
+    p2pconn.EnablePcapAll(tcp_variant+"_"+zis+"_wired_capture",false);
+    phy.EnablePcapAll(tcp_variant+"_"+zis+"_wireless_capture",false);
     
     Simulator::Stop(Seconds(10.0));
     Simulator::Run();
-    
-    // Calculate collision percentage
-    double collisionpercentage=(totalcollisions / (double)(totalreceptions+totalcollisions))*100;
-    std::cout << "Average Throughput: " << totalreceptions/nw << "packets" <<std::endl;
-    std::cout << "Collision Percentage: " << collisionpercentage << "%" << std::endl;
-    std::cout<<"Average Response Time: "<<resp_time/packet_cnt<<" microseconds"<<std::endl;
-
     Simulator::Destroy();
 }
 
@@ -219,7 +117,7 @@ int main(){
    for(int i=0;i<5;i++){
         for(int j=0;j<7;j++){
         
-            std::cout<<tcpvars[i]<<std::endl;
+            std::cout<<tcpvars[i], datasizes[j]<<std::endl;
             simulation(datasizes[j],tcpvars[i]);
             }
         }
